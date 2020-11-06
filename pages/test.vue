30ef82a998dbd8e523e1ea2dffd916b646254f72
SOKHUONG <uonsokhuong18@kit.edu.kh>
2020-11-06 21:54:28 +0700

    ✨ transition

---
+++ pages/test.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="w-screen h-screen bg-gray-800">
+
+	<div class="w-88 h-24 flexcontainer text-4xl">
+		<nuxt-link to="/" class="text-gray-300">Home</nuxt-link>
+	</div>
+
+
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
