851365c772a4ddff290e0701461ed99727323bb7
SOKHUONG <uonsokhuong18@kit.edu.kh>
2020-11-06 21:53:34 +0700

    🔥 un-import some files

--- plugins/prism.js
+++ plugins/prism.js
@@ -12,8 +12,8 @@ import 'prismjs/components/prism-scss';
 // import 'prismjs/components/prism-ruby';
 
 // // Include the toolbar plugin: (optional)
-import 'prismjs/plugins/toolbar/prism-toolbar';
-import 'prismjs/plugins/toolbar/prism-toolbar.css';
+// import 'prismjs/plugins/toolbar/prism-toolbar';
+// import 'prismjs/plugins/toolbar/prism-toolbar.css';
 
 // // Include the autolinker plugin: (optional)
 // import 'prismjs/plugins/autolinker/prism-autolinker';
@@ -28,9 +28,9 @@ import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
 // import 'prismjs/plugins/line-highlight/prism-line-highlight.css';
 
 // // Include some other plugins: (optional)
-import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard';
+// import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard';
 // import 'prismjs/plugins/highlight-keywords/prism-highlight-keywords';
-import 'prismjs/plugins/show-language/prism-show-language';
+// import 'prismjs/plugins/show-language/prism-show-language';
 import 'prismjs/plugins/normalize-whitespace/prism-normalize-whitespace';
 
 Vue.component("prism", Prism);
