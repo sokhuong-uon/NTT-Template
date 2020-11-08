a42a6db014dec3ef38273c6738dba3b41afec66d
SOKHUONG <uonsokhuong18@kit.edu.kh>
2020-11-08 08:53:26 +0700

    ✅ components test

---
+++ test/Components.spec.js
@@ -0,0 +1,16 @@
+import { mount, shallowMount } from '@vue/test-utils';
+
+import PopUp from '@/components/PopUp.vue';
+import Scene from '@/components/Scene.vue';
+
+const components = [PopUp, Scene];
+
+components.forEach(component => {
+
+	test('should has name', () => {
+
+		expect(component.name).toBeTruthy();
+
+	});
+
+});
